6abda31a5f784900cafcaff94184f4aa53c7660f
Sebastian Kalinowski <sebastian@kalinowski.eu>
2019-10-09 23:00:18 +0200

    Updates

---
+++ precache-manifest.97f56d3de3020e235fcf2557bfbbd9d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75de28c9a10d161d0db99354517a75d",
+    "url": "/osm24/index.html"
+  },
+  {
+    "revision": "21b0a573f9d377ce704b",
+    "url": "/osm24/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c6140cef528a83ec222c",
+    "url": "/osm24/static/js/2.dfb8812b.chunk.js"
+  },
+  {
+    "revision": "21b0a573f9d377ce704b",
+    "url": "/osm24/static/js/main.ca16238f.chunk.js"
+  },
+  {
+    "revision": "7bec95007c105292f1a5",
+    "url": "/osm24/static/js/runtime-main.18e4e97c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/osm24/static/media/logo.25bf045c.svg"
+  }
+]);
